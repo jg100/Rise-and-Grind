8db13efa6cb2346f6ea78641cf87734a62e21874
jg100 <jgiannelli13@gmail.com>
2020-02-15 10:08:12 -0800

    Final: Works

--- Dictionary/src/Dictionary.java
+++ Dictionary/src/Dictionary.java
@@ -2,6 +2,7 @@
  * Creator: Joel Giannelli
  */
 
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.Scanner;
 
@@ -9,6 +10,8 @@ public class Dictionary {
     public static void main(String[] args) {
 
         ArrayList<dataFile> dataLoad = new ArrayList<dataFile>();
+        ArrayList<dataFile> backUpData = new ArrayList<dataFile>();
+
 
         try {
             System.out.println("!Loading Data...");
@@ -17,38 +20,118 @@ public class Dictionary {
 
             }
         } catch (Exception e) {
-            System.out.println("An error has ocured. Please restart the program and try again ");
+            System.out.println("An error has occurred. Please restart the program and try again ");
         }
+        backUpData = (ArrayList<dataFile>) dataLoad.clone();
 
         System.out.println("!Loading completed...");
-        //CHECKING MY WORK TO MAKE SURE ARRAY-LIST IS THE CORRECT SIZE
-        System.out.println(dataLoad);
+
+        ArrayList<String> posList = new ArrayList<String>();
+        for(dataFile data : dataLoad) {
+            posList.add(data.getPartOfSpeech());
+        }
 
 
-        //User input/search
         Scanner scan = new Scanner(System.in);
-        String userInput = null;
         System.out.println("----DICTIONARY 340 JAVA------");
 
-
-        //looping while to make sure search continues until canceled by user
         while (true) {
-            System.out.print("Search: ");
-            userInput = scan.nextLine().toLowerCase();
+            dataLoad = (ArrayList<dataFile>) backUpData.clone();
 
-            System.out.println("|");
-            //Basic word searching return
-            for (dataFile word : dataLoad) {
-                if (word.getWord().equalsIgnoreCase(userInput) ){
-                    System.out.println(word.getWord() + ": " + " [" + word.getPartOfSpeech() + "] :" + word.getDefinition());
-                }
+            System.out.print("Search: ");
+            String userInput = scan.nextLine().toLowerCase();
+            String wordsUsed[] = {null,null,null};
+            wordsUsed = userInput.split(" ");
+            String searchWord = wordsUsed[0];
+            if(searchWord.equalsIgnoreCase("!Q")) {
+                System.out.println("----THANK YOU-----");
+                break;
             }
+
+            if(wordsUsed.length == 3 || wordsUsed.length == 2) {
+
+                if (!validEntry(wordsUsed[1],posList) ) {
+                    continue;
+                }
+
+                      if(wordsUsed.length ==  2) {
+                          if (wordsUsed[1].equalsIgnoreCase("distinct")) {
+                              dataLoad = (ArrayList<dataFile>) distinct(dataLoad).clone();
+
+                          }
+                          if(posList.contains(wordsUsed[1])) {
+                              dataLoad = partOfSpeechFinder(wordsUsed[1],dataLoad);
+                          }
+
+                        }
+                      if(wordsUsed.length == 3) {
+
+                          if(posList.contains(wordsUsed[1])) {
+                              dataLoad = (ArrayList<dataFile>) partOfSpeechFinder(wordsUsed[1],dataLoad).clone();
+                          }
+                          if (wordsUsed[2].equalsIgnoreCase("distinct") || wordsUsed[1].equalsIgnoreCase("distinct")) {
+                              dataLoad = (ArrayList<dataFile>) distinct(dataLoad).clone();
+
+                          }
+
+                      }
+            }
+            System.out.println("|");
+            int foundCount = 0;
+            for (dataFile word : dataLoad) {
+                if (word.getWord().equalsIgnoreCase(searchWord)) {
+                    System.out.println(word.getWord() + ": " + " [" + word.getPartOfSpeech() + "] :" + word.getDefinition());
+                    foundCount++;
+                }
+
+
+            }
+            if (foundCount == 0) {
+                System.out.println("<Not Found>");
+            }
+            System.out.println("|");
         }
-       // System.out.println("------THANK YOU-------");
     }
 
 
+    private static boolean validEntry( String x, ArrayList<String> posList) {
+        if(posList.contains(x) || x.equalsIgnoreCase("distinct")) {
+            return true;
+        }
+
+        System.out.println("|\n<2nd Argument must be a part of speech or distinct>\n|");
+        return false;
+    }
+
+    private static ArrayList<dataFile> distinct(ArrayList<dataFile> dataLoad) {
+        ArrayList<dataFile> placeHolder = (ArrayList<dataFile>) dataLoad.clone();
+
+        for (int i = 0; i < placeHolder.size(); i++) {
+            dataFile currentObject = placeHolder.get(i);
+
+            for (int k = i + 1; k < placeHolder.size(); k++) {
+                dataFile comparedObject = placeHolder.get(k);
+                if (currentObject.getDefinition().equals(comparedObject.getDefinition()) &&
+                currentObject.getPartOfSpeech().equals(comparedObject.getPartOfSpeech())) {
+                    placeHolder.remove(comparedObject);
+                    k--;
+                }
+            }
+
+        }
+
+        return placeHolder;
+    }
+    public static ArrayList<dataFile> partOfSpeechFinder(String POS, ArrayList<dataFile> dataLoad) {
+
+        ArrayList<dataFile> placeHolder = new ArrayList<dataFile>();
+
+        for(int i = 0; i < dataLoad.size(); i++) {
+            dataFile currentData = dataLoad.get(i);
+            if(currentData.getPartOfSpeech().equalsIgnoreCase(POS)) {
+                placeHolder.add(currentData);
+            }
+        }
+        return placeHolder;
+    }
 }
-
-
-
--- Dictionary/src/dataFile.java
+++ Dictionary/src/dataFile.java
@@ -5,16 +5,52 @@
  */
     public enum dataFile {
         //Predefined instances
-        DISTINCT1("Distinct", "adjective", "Worked in Java"),
-        DISTINCT2("Distinct", "adjective", "Unique. No Duplicates. Clearly different or of a different kind."),
-        DISTINCT3("Distinct", "adverb", "Uniquely. Written \"Distinctly\" "),
-        DISTINCT4("Distinct", "noun", "A keyword in this assignment"),
-        DISTINCT5("Distinct", "noun", "Distinct is a parameter in this assignment"),
 
-        CSC340_1("csc340", "adjective", "C++ version of CSC210 + CSC220 + more"),
-        CSC340_2("csc340", "noun", "A CS upper division course"),
-        CSC340_3("csc340", "noun", "Many hours outside of class"),
-        CSC340_4("csc340", "noun", "Programing Methodology");
+    ARROW1("Arrow", "noun", "Here is one arrow: <IMG> -=>> </IMG>"),
+
+    DISTINCT1("Distinct", "adjective", "Familiar. Worked in Java"),
+    DISTINCT2("Distinct", "adjective", "Unique. No Duplicates. Clearly different or of a different kind."),
+    DISTINCT3("Distinct", "adverb", "Uniquely. Written \"distinctly\"."),
+    DISTINCT4("Distinct", "noun", "A keyword in this assignment."),
+    DISTINCT5("Distinct", "noun", "A keyword in this assignment."),
+    DISTINCT6("Distinct", "noun", "A keyword in this assignment."),
+    DISTINCT7("Distinct", "noun", "An advanced search option."),
+    DISTINCT8("Distinct", "noun", "Distinct is a parameter in this assignment."),
+
+    PLACEHOLDER1("Placeholder", "adjective", "To be updated..."),
+    PLACEHOLDER2("Placeholder", "adjective" , "To be updated..."),
+    PLACEHOLDER3("Placeholder", "adverb" , "To be updated..."),
+    PLACEHOLDER4("Placeholder", "conjunction" , "To be updated..."),
+    PLACEHOLDER5("Placeholder", "interjection" , "To be updated..."),
+    PLACEHOLDER6("Placeholder", "noun" , "To be updated..."),
+    PLACEHOLDER7("Placeholder", "noun" , "To be updated..."),
+    PLACEHOLDER8("Placeholder", "noun" , "To be updated..."),
+    PLACEHOLDER9("Placeholder", "preposition" , "To be updated..."),
+    PLACEHOLDER10("Placeholder", "pronoun" , "To be updated..."),
+    PLACEHOLDER11("Placeholder", "verb" , "To be updated..."),
+
+    CSC340_1("CSC340", "adjective", "C++ version of CSC210 + CSC220 + more."),
+    CSC340_2("CSC340", "noun", "A CS upper division course."),
+    CSC340_3("CSC340", "noun", "Many hours outside of class."),
+    CSC340_4("CSC340", "noun", "Programing Methodology."),
+
+    CSC220_1("CSC220", "adjective", "Ready to create complex data structures."),
+    CSC220_2("CSC220", "verb", "To create data structures."),
+
+    BOOK1("Book", "noun", "A set of pages."),
+    BOOK2("Book", "noun", "A written work published in printed or electronic form."),
+    BOOK3("Book", "verb", "To arrange for someone to have a seat on a plane."),
+    BOOK4("Book", "verb", "To arrange something on a particular date."),
+
+    ADVERB1("Adverb", "noun", "Adverb is a word that adds more information about place, time, manner, cause or degree to a verb, an adjective, a phrase or another adverb."),
+
+    ADJECTIVE1("Adjective", "noun", "Adjective is a word that describes a person or thing, for example big, red and clever in a big house, red wine and a clever idea."),
+
+    //CONJUNCTION1(),
+
+    INTERJECTION1("Interjection", "noun", "Interjection is a short sound, word or phrase spoken suddenly to express an emotion. Oh!, Look out! and Ow! are interjections."),
+
+    NOUN1("Noun", "noun", "Noun is a word that refers to a person, (such as Ann or doctor), a place (such as Paris or a city) or a thing, a quality or an activity (such as plant, sorrow, or tennis.");
 
         /**
          * FILL IN REST OF DATA AS SOON AS FIRST PART IS WORKING
@@ -49,7 +85,3 @@
         //end getter methods
 
     }
-
-
-}
-
